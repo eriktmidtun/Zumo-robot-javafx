a3b8e098a42ae7c8156e668fde792af94a6f3d43
Erik Turøy Midtun <etm.midtun@gmail.com>
2019-03-15 13:42:44 +0100

    startet javafx del

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.Event;
+
+public class Controller {
+    public void connect(Event event){
+        System.out.println("test bt c");
+    }
+    public void disconnect(Event event){
+        System.out.println("test bt d");
+    }
+}
